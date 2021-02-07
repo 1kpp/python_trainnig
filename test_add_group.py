45d828a3c96f5dff8f321b870a512e8e0ea0527d
Shurygin_M <kpp11025@gmail.com>
2021-02-07 11:21:09 +0300

    Created supporting methods and added parameters into them

--- test_add_group.py
+++ test_add_group.py
@@ -1,56 +1,69 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
     
-    def test_untitled_test_case(self):
+    def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        #Log in
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_id("LoginForm").submit()
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group_section(wd)
+        self.create_group(wd, group_name="asdsad", header="13123123", footer="asdsadd")
+        self.return_to_groups(wd)
+        self.lodout(wd)
 
-        #Open a group section and create a new one
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group_section(wd)
+        self.create_group(wd, group_name="", header="", footer="")
+        self.return_to_groups(wd)
+        self.lodout(wd)
+
+
+    def lodout(self, wd):
+        # Logout
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd, group_name, header, footer):
+        # Create a new group
         wd.find_element_by_name("new").click()
+        # Fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("asdsad")
+        wd.find_element_by_name("group_name").send_keys(group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("13123123")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("asdsadd")
+        wd.find_element_by_name("group_footer").send_keys(footer)
+        # Submit group creation
         wd.find_element_by_name("submit").click()
 
-        #Open a group section and check whether a created one there
+    def open_group_section(self, wd):
         wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_link_text("home").click()
-        wd.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
+
+    def login(self, wd, username, password):
+        # Log in
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_id("LoginForm").submit()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
         self.wd.quit()
