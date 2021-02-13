57910bcbd266b7ecb8e228fd36ec5c1849ed97d9
1kpp <kpp11025@gmail.com>
2021-02-13 14:01:05 +0300

    In the file "test_add_contact.py" fixtures were added. Fixtures are represented in the new class "applications" which can be found in file "application_contact.py"

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,78 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
+import pytest
 from contact import Contact
+from application_contact import Application
 
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@ pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
     
-    def test_add_new_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.create_contact(wd, Contact(firstname="Vladimir",
-                       middlename="Ilich", lastname="Ulyanov", nickname="Lenin", company="RSDRP", zagolovok="zagolovok",
-                       address="Saint-Petersburg", home="4815162342", mobile="4815162342", work="4815162342",
-                       fax="4815162342", email="no email", email2="no email", email3="no email", homepage="no site",
-                       bday="22", bmonth="April", byear="1870", aday="22", amonth="April", ayear="2020",
-                       address2="Soviet country", phone2="Soviet country", notes="Soviet country"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
+def test_add_new_contact(app):
+    app.open_home_page()
+    app.login( username="admin", password="secret")
+    app.create_contact(Contact(firstname="Vladimir",
+                   middlename="Ilich", lastname="Ulyanov", nickname="Lenin", company="RSDRP", zagolovok="zagolovok",
+                   address="Saint-Petersburg", home="4815162342", mobile="4815162342", work="4815162342",
+                   fax="4815162342", email="no email", email2="no email", email3="no email", homepage="no site",
+                   bday="22", bmonth="April", byear="1870", aday="22", amonth="April", ayear="2020",
+                   address2="Soviet country", phone2="Soviet country", notes="Soviet country"))
+    app.return_to_home_page()
+    app.logout()
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home").click()
-
-    def create_contact(self, wd, contact):
-        #add new contact
-        wd.find_element_by_link_text("add new").click()
-        #filling the form
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").send_keys(contact.zagolovok)
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").send_keys(contact.work)
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        #submit
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-
-    def login(self, wd, username, password):
-        #enter login, password and submit
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("LoginForm").submit()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
