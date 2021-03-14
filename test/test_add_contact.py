b0c5e70110543958f97eaa8e3cbf1e8d0ce52aef
1kpp <kpp11025@gmail.com>
2021-03-14 14:31:25 +0300

    Added two test generators and test data is being uploaded from json-files
    Two test generators ( for groups and for contacts) generate test data in a json files. From there tests draw test data

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,24 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testData = [Contact(firstname="", middlename="", lastname="", nickname="", company="")] + [
-    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 20),
-            lastname=random_string("lastname", 20), nickname=random_string("nickname", 15), company=random_string("company", 15))
-    for i in range(5)
-]
-
-
-@pytest.mark.parametrize("contact", testData, ids=[repr(x) for x in testData])
-def test_add_new_contact(app, contact):
+def test_add_new_contact(app, json_contacts):
+    contact = json_contacts
     old_list = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_list) + 1 == app.contact.count()
